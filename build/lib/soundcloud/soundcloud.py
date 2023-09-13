ca3f6b8362aa80cd6db518b83a3eed6befc3eae3
Kelsey Way <kelsey@blinq.me>
2023-09-13 22:58:54 +1000

    package with api-mobile base url

--- build/lib/soundcloud/soundcloud.py
+++ build/lib/soundcloud/soundcloud.py
@@ -9,10 +9,10 @@ except ImportError:
     # get_args and get_origin shim for version < 3.8
     def get_args(tp):
         return getattr(tp, "__args__", ())
-
+    
     def get_origin(tp):
         return getattr(tp, "__origin__", None)
-
+    
 from urllib.parse import parse_qs, urljoin, urlparse
 
 import requests
@@ -35,47 +35,41 @@ T = TypeVar("T")
 class ClientIDGenerationError(Exception):
     pass
 
-
+        
 class SoundCloud:
-
+    
     DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0"
-    ASSETS_SCRIPTS_REGEX = re.compile(
-        r"src=\"(https:\/\/a-v2\.sndcdn\.com/assets/[^\.]+\.js)\"")
+    ASSETS_SCRIPTS_REGEX = re.compile(r"src=\"(https:\/\/a-v2\.sndcdn\.com/assets/[^\.]+\.js)\"")
     CLIENT_ID_REGEX = re.compile(r"client_id:\"([^\"]+)\"")
-
+    
     def __init__(self, client_id: str = None, auth_token: str = None, user_agent: str = DEFAULT_USER_AGENT) -> None:
-
+        
         if not client_id:
             client_id = self.generate_client_id()
-
+        
         self.client_id = client_id
         self.auth_token = None
         self.authorization = None
         self.user_agent = user_agent
         self.set_auth_token(auth_token)
-
+        
         self.requests: Dict[str, Request] = {
             "me":                         Request[User](self, "/me", User),
             "me_stream":                  CollectionRequest[StreamItem](self, "/stream", StreamItem),
             "resolve":                    Request[SearchItem](self, "/resolve", SearchItem),
             "search":                     CollectionRequest[SearchItem](self, "/search", SearchItem),
-            # ?filter.genre_or_tag
-            "search_albums":              CollectionRequest[AlbumPlaylist](self, "/search/albums", AlbumPlaylist),
+            "search_albums":              CollectionRequest[AlbumPlaylist](self, "/search/albums", AlbumPlaylist), # ?filter.genre_or_tag
             "search_playlists":           CollectionRequest[AlbumPlaylist](self, "/search/playlists_without_albums", AlbumPlaylist),
-            # ?filter.created_at&filter.duration&filter.license
-            "search_tracks":              CollectionRequest[Track](self, "/search/tracks", Track),
-            # ?filter.place
-            "search_users":               CollectionRequest[User](self, "/search/users", User),
+            "search_tracks":              CollectionRequest[Track](self, "/search/tracks", Track), #?filter.created_at&filter.duration&filter.license
+            "search_users":               CollectionRequest[User](self, "/search/users", User), #?filter.place
             "tag_recent_tracks":          CollectionRequest[Track](self, "/recent-tracks/{tag}", Track),
             "playlist":                   Request[BasicAlbumPlaylist](self, "/playlists/{playlist_id}", BasicAlbumPlaylist),
             "playlist_likers":            CollectionRequest[User](self, "/playlists/{playlist_id}/likers", User),
             "playlist_reposters":         CollectionRequest[User](self, "/playlists/{playlist_id}/reposters", User),
             "track":                      Request[BasicTrack](self, "/tracks/{track_id}", BasicTrack),
             "tracks":                     ListRequest[BasicTrack](self, "/tracks", BasicTrack),
-            # (can be representation=mini)
-            "track_albums":               CollectionRequest[BasicAlbumPlaylist](self, "/tracks/{track_id}/albums", BasicAlbumPlaylist),
-            # (can be representation=mini)
-            "track_playlists":            CollectionRequest[BasicAlbumPlaylist](self, "/tracks/{track_id}/playlists_without_albums", BasicAlbumPlaylist),
+            "track_albums":               CollectionRequest[BasicAlbumPlaylist](self, "/tracks/{track_id}/albums", BasicAlbumPlaylist), # (can be representation=mini)
+            "track_playlists":            CollectionRequest[BasicAlbumPlaylist](self, "/tracks/{track_id}/playlists_without_albums", BasicAlbumPlaylist), # (can be representation=mini)
             "track_comments":             CollectionRequest[BasicComment](self, "/tracks/{track_id}/comments", BasicComment),
             "track_likers":               CollectionRequest[User](self, "/tracks/{track_id}/likers", User),
             "track_reposters":            CollectionRequest[User](self, "/tracks/{track_id}/reposters", User),
@@ -94,23 +88,21 @@ class SoundCloud:
             "user_stream":                CollectionRequest[StreamItem](self, "/stream/users/{user_id}", StreamItem),
             "user_tracks":                CollectionRequest[BasicTrack](self, "/users/{user_id}/tracks", BasicTrack),
             "user_toptracks":             CollectionRequest[BasicTrack](self, "/users/{user_id}/toptracks", BasicTrack),
-            # (can be representation=mini)
-            "user_albums":                CollectionRequest[BasicAlbumPlaylist](self, "/users/{user_id}/albums", BasicAlbumPlaylist),
-            # (can be representation=mini)
-            "user_playlists":             CollectionRequest[BasicAlbumPlaylist](self, "/users/{user_id}/playlists_without_albums", BasicAlbumPlaylist),
+            "user_albums":                CollectionRequest[BasicAlbumPlaylist](self, "/users/{user_id}/albums", BasicAlbumPlaylist), # (can be representation=mini)
+            "user_playlists":             CollectionRequest[BasicAlbumPlaylist](self, "/users/{user_id}/playlists_without_albums", BasicAlbumPlaylist), # (can be representation=mini)
             "user_web_profiles":          ListRequest[WebProfile](self, "/users/{user_urn}/web-profiles", WebProfile)
         }
-
+    
     def set_auth_token(self, auth_token: str) -> None:
         if auth_token is not None:
             if auth_token.startswith("OAuth"):
                 auth_token = auth_token.split()[-1]
             self.auth_token = auth_token
             self.authorization = f"OAuth {auth_token}" if auth_token else None
-
+    
     def get_default_headers(self) -> Dict[str, str]:
         return {"User-Agent": self.user_agent}
-
+    
     def generate_client_id(self) -> str:
         r = requests.get("https://soundcloud.com")
         r.raise_for_status()
@@ -122,18 +114,16 @@ class SoundCloud:
         r.raise_for_status()
         client_id = self.CLIENT_ID_REGEX.search(r.text)
         if not client_id:
-            raise ClientIDGenerationError(
-                f"Could not find client_id in script '{url}'")
+            raise ClientIDGenerationError(f"Could not find client_id in script '{url}'")
         return client_id.group(1)
-
+    
     def is_client_id_valid(self) -> bool:
         """
         Checks if current client_id is valid
         """
         try:
             next(
-                self.requests["tag_recent_tracks"](
-                    tag="electronic", limit="1", use_auth=False)
+                self.requests["tag_recent_tracks"](tag="electronic", limit="1", use_auth=False)
             )
             return True
         except HTTPError as err:
@@ -154,89 +144,89 @@ class SoundCloud:
                 return False
             else:
                 raise
-
+    
     def get_me(self) -> User:
         """
         Gets the user associated with client's auth token
         """
         return self.requests["me"]()
-
+    
     def get_my_stream(self, **kwargs) -> Generator[StreamItem, None, None]:
         """
         Returns the stream of recent uploads/reposts
         for the client's auth token
         """
         return self.requests["me_stream"](**kwargs)
-
+        
     def resolve(self, url: str) -> Optional[SearchItem]:
         """
         Returns the resource at the given URL if it
         exists, otherwise return None
         """
         return self.requests["resolve"](url=url)
-
+    
     def search(self, query: str, **kwargs) -> Generator[SearchItem, None, None]:
         """
         Search for users, tracks, and playlists
         """
         return self.requests["search"](q=query, **kwargs)
-
+        
     def search_albums(self, query: str, **kwargs) -> Generator[AlbumPlaylist, None, None]:
         """
         Search for albums (not playlists)
         """
         return self.requests["search_albums"](q=query, **kwargs)
-
+        
     def search_playlists(self, query: str, **kwargs) -> Generator[AlbumPlaylist, None, None]:
         """
         Search for playlists
         """
         return self.requests["search_playlists"](q=query, **kwargs)
-
+        
     def search_tracks(self, query: str, **kwargs) -> Generator[Track, None, None]:
         """
         Search for tracks
         """
         return self.requests["search_tracks"](q=query, **kwargs)
-
+        
     def search_users(self, query: str, **kwargs) -> Generator[User, None, None]:
         """
         Search for users
         """
         return self.requests["search_users"](q=query, **kwargs)
-
+        
     def get_tag_tracks_recent(self, tag: str, **kwargs) -> Generator[Track, None, None]:
         """
         Get most recent tracks for this tag
         """
         return self.requests["tag_recent_tracks"](tag=tag, **kwargs)
-
+        
     def get_playlist(self, playlist_id: int) -> Optional[BasicAlbumPlaylist]:
         """
         Returns the playlist with the given playlist_id.
         If the ID is invalid, return None
         """
         return self.requests["playlist"](playlist_id=playlist_id)
-
+        
     def get_playlist_likers(self, playlist_id: int, **kwargs) -> Generator[User, None, None]:
         """
         Get people who liked this playlist
         """
         return self.requests["playlist_likers"](playlist_id=playlist_id, **kwargs)
-
+        
     def get_playlist_reposters(self, playlist_id: int, **kwargs) -> Generator[User, None, None]:
         """
         Get people who reposted this playlist
         """
         return self.requests["playlist_reposters"](playlist_id=playlist_id, **kwargs)
-
+    
     def get_track(self, track_id: int) -> Optional[BasicTrack]:
         """
         Returns the track with the given track_id.
         If the ID is invalid, return None
         """
         return self.requests["track"](track_id=track_id)
-
+    
     def get_tracks(self, track_ids: List[int], playlistId: int = None, playlistSecretToken: str = None, **kwargs) -> List[BasicTrack]:
         """
         Returns the tracks with the given track_ids.
@@ -247,63 +237,61 @@ class SoundCloud:
         if playlistSecretToken:
             kwargs["playlistSecretToken"] = playlistSecretToken
         return self.requests["tracks"](ids=",".join([str(id) for id in track_ids]), **kwargs)
-
+    
     def get_track_albums(self, track_id: int, **kwargs) -> Generator[AlbumPlaylist, None, None]:
         """
         Get albums that this track is in
         """
         return self.requests["track_albums"](track_id=track_id, **kwargs)
-
+        
     def get_track_playlists(self, track_id: int, **kwargs) -> Generator[AlbumPlaylist, None, None]:
         """
         Get playlists that this track is in
         """
         return self.requests["track_playlists"](track_id=track_id, **kwargs)
-
+        
     def get_track_comments(self, track_id: int, threaded: int = 0, filter_replies: int = 1, **kwargs) -> Generator[BasicComment, None, None]:
         """
         Get comments on this track
         """
-        return self.requests["track_comments"](track_id=track_id,
+        return self.requests["track_comments"](track_id=track_id, 
                                                threaded=threaded,
                                                filter_replies=filter_replies,
                                                **kwargs)
-
+        
     def get_track_likers(self, track_id: int, **kwargs) -> Generator[User, None, None]:
         """
         Get users who liked this track
         """
         return self.requests["track_likers"](track_id=track_id, **kwargs)
-
+        
     def get_track_reposters(self, track_id: int, **kwargs) -> Generator[User, None, None]:
         """
         Get users who reposted this track
         """
         return self.requests["track_reposters"](track_id=track_id, **kwargs)
-
+        
     def get_track_original_download(self, track_id: int, token: str = None) -> Optional[str]:
         """
         Get track original download link. If track is private,
         requires secret token to be provided (last part of secret URL)
         """
         if token:
-            download = self.requests["track_original_download"](
-                track_id=track_id, secret_token=token)
+            download = self.requests["track_original_download"](track_id=track_id, secret_token=token)
         else:
-            download = self.requests["track_original_download"](
-                track_id=track_id)
+            download = self.requests["track_original_download"](track_id=track_id)
         if download is None:
             return None
         else:
             return download.redirectUri
-
+            
     def get_user(self, user_id: int) -> Optional[User]:
         """
         Returns the user with the given user_id.
         If the ID is invalid, return None
         """
         return self.requests["user"](user_id=user_id)
-
+    
     def get_user_by_username(self, username: str) -> Optional[User]:
         """
         Returns the user with the given username.
@@ -314,13 +302,13 @@ class SoundCloud:
             return resource
         else:
             return None
-
+    
     def get_user_comments(self, user_id: int, **kwargs) -> Generator[Comment, None, None]:
         """
         Get comments by this user
         """
         return self.requests["user_comments"](user_id=user_id, **kwargs)
-
+    
     def get_conversation_messages(self, user_id: int, conversation_id: int, **kwargs) -> Generator[Message, None, None]:
         """
         Get messages in this conversation
@@ -328,19 +316,19 @@ class SoundCloud:
         return self.requests["user_conversation_messages"](user_id=user_id,
                                                            conversation_id=conversation_id,
                                                            **kwargs)
-
+        
     def get_conversations(self, user_id: int, **kwargs) -> Generator[Conversation, None, None]:
         """
         Get conversations including this user
         """
         return self.requests["user_conversations"](user_id=user_id, **kwargs)
-
+    
     def get_unread_conversations(self, user_id: int, **kwargs) -> Generator[Conversation, None, None]:
         """
         Get conversations unread by this user
         """
         return self.requests["user_conversations_unread"](user_id=user_id, **kwargs)
-
+        
     def get_user_featured_profiles(self, user_id: int, **kwargs) -> Generator[User, None, None]:
         """
         Get profiles featured by this user
@@ -358,56 +346,56 @@ class SoundCloud:
         Get users this user is following
         """
         return self.requests["user_followings"](user_id=user_id, **kwargs)
-
+        
     def get_user_likes(self, user_id: int, **kwargs) -> Generator[Like, None, None]:
         """
         Get likes by this user
         """
         return self.requests["user_likes"](user_id=user_id, **kwargs)
-
+        
     def get_user_related_artists(self, user_id: int, **kwargs) -> Generator[User, None, None]:
         """
         Get artists related to this user
         """
         return self.requests["user_related_artists"](user_id=user_id, **kwargs)
-
+        
     def get_user_reposts(self, user_id: int, **kwargs) -> Generator[RepostItem, None, None]:
         """
         Get reposts by this user
         """
         return self.requests["user_reposts"](user_id=user_id, **kwargs)
-
+        
     def get_user_stream(self, user_id: int, **kwargs) -> Generator[StreamItem, None, None]:
         """
         Returns generator of track uploaded by given user and
         reposts by this user
         """
         return self.requests["user_stream"](user_id=user_id, **kwargs)
-
+    
     def get_user_tracks(self, user_id: int, **kwargs) -> Generator[BasicTrack, None, None]:
         """
         Get tracks uploaded by this user
         """
         return self.requests["user_tracks"](user_id=user_id, **kwargs)
-
+    
     def get_user_popular_tracks(self, user_id: int, **kwargs) -> Generator[BasicTrack, None, None]:
         """
         Get popular tracks uploaded by this user
         """
         return self.requests["user_toptracks"](user_id=user_id, **kwargs)
-
+        
     def get_user_albums(self, user_id: int, **kwargs) -> Generator[BasicAlbumPlaylist, None, None]:
         """
         Get albums uploaded by this user
         """
         return self.requests["user_albums"](user_id=user_id, **kwargs)
-
+        
     def get_user_playlists(self, user_id: int, **kwargs) -> Generator[BasicAlbumPlaylist, None, None]:
         """
         Get playlists uploaded by this user
         """
         return self.requests["user_playlists"](user_id=user_id, **kwargs)
-
+    
     def get_user_links(self, user_urn: str, **kwargs) -> List[WebProfile]:
         """
         Get links in this user's description
@@ -417,20 +405,20 @@ class SoundCloud:
 
 @dataclass
 class Request(Generic[T]):
-    base = "https://api-mobile.soundcloud.com"
+    
+    base = "https://api-v2.soundcloud.com"
     client: SoundCloud
     format_url: str
     return_type: T
-
+    
     def format_url_and_remove_params(self, kwargs):
-        format_args = {tup[1] for tup in string.Formatter().parse(
-            self.format_url) if tup[1] is not None}
+        format_args = {tup[1] for tup in string.Formatter().parse(self.format_url) if tup[1] is not None}
         args = {}
         for k in list(kwargs.keys()):
             if k in format_args:
-                args[k] = kwargs.pop(k)
+                args[k] = kwargs.pop(k)     
         return self.base + self.format_url.format(**args)
-
+    
     def convert_dict(self, d):
         union = get_origin(self.return_type) is Union
         if union:
@@ -442,7 +430,7 @@ class Request(Generic[T]):
         else:
             return self.return_type.from_dict(d)
         raise ValueError(f"Could not convert {d} to type {self.return_type}")
-
+    
     def __call__(self, use_auth=True, **kwargs) -> Optional[T]:
         """
         Requests the resource at the given url with
@@ -465,7 +453,7 @@ class Request(Generic[T]):
 
 @dataclass
 class CollectionRequest(Request, Generic[T]):
-
+    
     def __call__(self, use_auth=True, offset: str = None, limit: int = None, **kwargs) -> Generator[T, None, None]:
         """
         Yields resources from the given url with
@@ -493,11 +481,10 @@ class CollectionRequest(Request, Generic[T]):
                 resource_url = data.get("next_href", None)
                 parsed = urlparse(resource_url)
                 params = parse_qs(parsed.query)
-                # next_href doesn't contain client_id
-                params["client_id"] = self.client.client_id
+                params["client_id"] = self.client.client_id # next_href doesn't contain client_id
                 resource_url = urljoin(resource_url, parsed.path)
-
-
+        
+                
 @dataclass
 class ListRequest(Request, Generic[T]):
     """
@@ -505,7 +492,6 @@ class ListRequest(Request, Generic[T]):
     parameters given by kwargs. Converts the resources
     to type T and returns them.
     """
-
     def __call__(self, use_auth=True, **kwargs) -> List[T]:
         resource_url = self.format_url_and_remove_params(kwargs)
         params = kwargs
